a718579e7c90a2c3803a453b9f5bd88e2c50bc13
OtavioFilipe <darksnack16@gmail.com>
2022-09-13 23:00:26 -0300

    import and configure tailwind :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return <button>{props.title}</button>;
-}
+import "./styles/main.css";
 
 function App() {
-  return (
-    <div>
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-    </div>
-  );
+  return <div></div>;
 }
 export default App;
